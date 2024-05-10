fc35dbe0e5bb95590666a132051964f175858492
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-05-10 18:17:49 +0530

    Added Zod Validation Successfully

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const connectDB = require("./db/index.js");
 const app = require("./app");
+const dotenv = require("dotenv");
 
 connectDB()
   .then(() => {
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,10 +6,12 @@ const {
   getUser,
 } = require("../controllers/user.controller");
 
+const RegisterSchema = require("../validation/auth_validators");
+const validate = require("../middlewares/validater.middleware");
 const router = Router();
 
 router.post("/login", loginUser);
-router.post("/register", registerUser);
+router.post("/register", validate(RegisterSchema),registerUser);
 router.get("/checkusername/:username", checkUsername);
 router.get("/get", getUser);
 
